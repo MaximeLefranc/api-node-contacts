5a7d84b32b1a425dfe957166d62da26ac76e5ae8
Maxime <maxilefranc@gmail.com>
2023-10-17 10:14:32 -0500

    add catchAsync function in helpers folder for catching all errors from server side

--- Controllers/ContactController.js
+++ Controllers/ContactController.js
@@ -1,11 +1,12 @@
 import Contact from '../Models/Contact.js';
+import catchAsync from '../helpers/catchAsync.js';
 
 const ContactController = {};
 
-ContactController.create = async (req, res) => {
+ContactController.create = catchAsync(async (req, res) => {
   console.log('req.body = ', req.body);
   const contact = await Contact.create(req.body);
   res.send(contact);
-};
+});
 
 export default ContactController;
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import contactRouter from './routes/contact.js';
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/carnet-adresses');
 
@@ -15,6 +19,15 @@ app.use('/contacts', contactRouter);
 
 app.use((req, res) => {
   res.status(400).send('Page non trouvée - Not Found Error 404');
+});
+
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .send(
+      'Une erreur interne du serveur vient de se produire, merci de réessayer ultérieurement.'
+    );
+  console.error(err);
 });
 
 app.listen(port, () => {
